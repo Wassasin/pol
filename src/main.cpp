eb43d283b70c52436593905ae5d4f7854133685e
Bas Westerbaan <bas@westerbaan.name>
2012-08-20 12:40:18 +0200

    c++/cmake/Boost stub
    Signed-off-by: Bas Westerbaan <bas@westerbaan.name>

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main(int argc, const char* argv[])
+{
+    cout << "Hi, I am Pol." << endl;
+    return 0;
+}
+
+// vim: et:sta:bs=2:sw=4:
